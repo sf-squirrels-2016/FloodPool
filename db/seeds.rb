e131004450752b9ce78544cfe39da5fc7073fe9f
DaveSpivey <dspive1@gmail.com>
2016-02-18 15:55:01 -0800

    Add initial show question route structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,7 @@ User.delete_all
 
 #create 10 dummy users
 users = 10.times.map do
-  User.create!( :first_name => Faker::Name.first_name,
-                :last_name  => Faker::Name.last_name,
+  User.create!( :name => Faker::Name.first_name,
                 :email      => Faker::Internet.email,
                 :password   => 'password' )
 end
