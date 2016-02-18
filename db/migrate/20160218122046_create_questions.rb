c131df275903e7487115db7a20e5b30bf904eaff
Monique Williamson <moniquewill1@yahoo.com>
2016-02-18 15:10:05 -0800

    Update 20160218122046_create_questions.rb

--- db/migrate/20160218122046_create_questions.rb
+++ db/migrate/20160218122046_create_questions.rb
@@ -2,6 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.references :user
+      t.string :title, null: false
       t.text :text, null: false
 
       t.timestamps null: false
